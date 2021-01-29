5cc7bd88311fc23cee17adf910101be51525b823
David Rodri <drodrigerrard@gmail.com>
2021-01-29 17:55:20 +0000

    Fix #1 express and cors implemented

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hiiiii')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+  origin: ['localhost:4200']
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.send('Hiiiii')
+})
+
+app.listen(5000, (err) => {
+  if(err) return console.log('Have an error! ', err);
+  console.log('Server running in port 5000'); 
+})
