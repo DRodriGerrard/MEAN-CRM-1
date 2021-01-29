4cfd6c3421420927562a42daebf6085c5a28add6
David Rodri <drodrigerrard@gmail.com>
2021-01-29 19:42:41 +0000

    Fix #5 customers controller with all functions implemented

---
+++ api/customers/customers.controller.js
@@ -0,0 +1,20 @@
+module.exports = {getAll, getOneById, createOne, putOne, patchOne, removeOne};
+
+function getAll(req, res) {
+  return res.send('Todo niiiiiice con el getAll')
+}
+function getOneById(req, res) {
+  return res.send('Todo niiiiiice con el getOneById')
+}
+function createOne(req, res) {
+  return res.send('Todo niiiiiice con el createOne')
+}
+function putOne(req, res) {
+  return res.send('Todo niiiiiice con el putOne')
+}
+function patchOne(req, res) {
+  return res.send('Todo niiiiiice con el patchOne')
+}
+function removeOne(req, res) {
+  return res.send('Todo niiiiiice con el removeOne')
+}
